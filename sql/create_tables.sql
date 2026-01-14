ba10e6debd55a2e6d1a20bc43d78093586b9587e
Pratikshya Chand <pratikshyachand07@gmail.com>
2026-01-15 00:33:32 +0545

    Created tables for analysis

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,20 +1,26 @@
 CREATE TABLE customers(
- customerID varchar(15) primary key,
- gender varchar(7),
- age int 
-)
+ CustomerID varchar(15) primary key,
+ FirstName varchar(20),
+ LastName varchar(20),
+ Country varchar(100)
+);
 
 CREATE TABLE products(
- product_category varchar(20) primary key,
-)
+  ProductID varchar(15) primary key,
+  Name varchar(100),
+  Category varchar(100),
+  Price decimal
+);
 
-CREATE TABLE sales(
- transactionID int primary key not null,
- salesDate date,
- price_per_unit decimal,
- customerID varchar(15) foreign key,
- product_category varchar(20) foreign key,
- quantity int,
- total_amount decimal
+CREATE TABLE sales( 
+ OrderID varchar(15) primary key,
+ CustomerID varchar(15),
+ ProductID varchar(15),
+ OrderDate date,
+ Quantity int,
+ UnitPrice decimal(10,2),
+ SalesAmount decimal(10,2),
+ foreign key (CustomerID) references customers(CustomerID),
+ foreign key (ProductID) references products(ProductID)
 
-)
+);
