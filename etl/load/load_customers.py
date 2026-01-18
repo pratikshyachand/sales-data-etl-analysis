ac615ee7eb9713fbaec7a6b91af22ad10e33f4af
Pratikshya Chand <pratikshyachand07@gmail.com>
2026-01-18 12:07:06 +0545

    Updated load_customers.py

--- etl/load/load_customers.py
+++ etl/load/load_customers.py
@@ -8,25 +8,15 @@ def LoadCustomers(df):
 
     cursor = con.cursor() 
   
-# ---- Customers ETL ----
-
-    df_customers = pd.read_csv('data/customers.csv')
-    df_customers = df_customers[['CustomerID', 'FirstName', 'LastName', 'Country']]
-
-
-    df_customers.drop_duplicates(inplace=True)
-
-    for _, row in df_customers.iterrows():
+    for _, row in df.iterrows():
       cursor.execute("""
         INSERT INTO customers(CustomerID, FirstName, LastName, Country)
            VALUES(?, ?, ?, ?)
       """, row['CustomerID'], row['FirstName'], row['LastName'], row['Country'])
 
-
     con.commit()
     cursor.close()
     con.close()
-
 
  except Exception as e:
    print(f"COnnection failed. {e}")
