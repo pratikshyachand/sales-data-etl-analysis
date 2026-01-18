84444392b2e04869e05b0e144300af2b40236fc0
Pratikshya Chand <pratikshyachand07@gmail.com>
2026-01-18 11:53:36 +0545

    Modified column data types

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,26 +1,25 @@
 CREATE TABLE customers(
- CustomerID varchar(15) primary key,
+ CustomerID int primary key,
  FirstName varchar(20),
  LastName varchar(20),
  Country varchar(100)
 );
 
 CREATE TABLE products(
-  ProductID varchar(15) primary key,
+  ProductID int primary key,
   Name varchar(100),
   Category varchar(100),
-  Price decimal
+  Price decimal(10,2)
 );
 
 CREATE TABLE sales( 
- OrderID varchar(15) primary key,
- CustomerID varchar(15),
- ProductID varchar(15),
+ OrderID int primary key,
+ CustomerID int,
+ ProductID int,
  OrderDate date,
  Quantity int,
  UnitPrice decimal(10,2),
  SalesAmount decimal(10,2),
  foreign key (CustomerID) references customers(CustomerID),
  foreign key (ProductID) references products(ProductID)
-
 );
