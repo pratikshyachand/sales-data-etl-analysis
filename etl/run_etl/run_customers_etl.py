a6c2c70001319c5c62852a74c53d9ba2af126df3
Pratikshya Chand <pratikshyachand07@gmail.com>
2026-01-19 12:11:47 +0545

    Deleted redundant files

--- etl/run_etl/run_customers_etl.py
+++
@@ -1,7 +0,0 @@
-from etl.extract import extract_data
-from etl.transform.transform_customers import TransformCustomers
-from etl.load.load_customers import LoadCustomers
-
-df = extract_data("data/customers.csv")
-df_cleaned = TransformCustomers(df)
-LoadCustomers(df_cleaned)
--- etl/run_etl/run_sales_etl.py
+++
@@ -1,7 +0,0 @@
-from etl.extract import extract_data
-from etl.transform.transform_sales import TransformSales
-from etl.load.load_sales import LoadSales
-
-df = extract_data("data/sales.csv")
-df_cleaned = TransformSales(df)
-LoadSales(df_cleaned)
