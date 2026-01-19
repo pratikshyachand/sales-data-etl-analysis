6318bca50ebc2d6f1324b98887245bcd9e6608bb
Pratikshya Chand <pratikshyachand07@gmail.com>
2026-01-19 12:06:20 +0545

    Updated extract.py to extract all CSVs at once

--- etl/extract.py
+++ etl/extract.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import os
 
-def extract_data(path):
-    return pd.read_csv(path)
+DATA_DIR = "data" # Folder containing CSVs
+
+def extract_all():
+    data = {}
+
+    data['customers'] = pd.read_csv(os.path.join(DATA_DIR, "customers.csv")) # creates path to customers.csv
+    data['products'] = pd.read_csv(os.path.join(DATA_DIR, "products.csv"))   # creates path to products.csv
+    data['sales'] = pd.read_csv(os.path.join(DATA_DIR, "sales.csv"))         # creates path to sales.csv
+
+    return data
+
+
+
